ca6703e56884965eb19e80f9df8bcb04940f9b9a
Ryan Atkinson <mail@ryanatkn.com>
2023-01-03 15:27:50 -0600

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '@feltcoop/felt/style.css';
+	import {base} from '$app/paths';
+
+	import './style.css';
+</script>
+
+<svelte:head>
+	<title>felt-template</title>
+	<link rel="icon" href="{base}/favicon.png" />
+</svelte:head>
+
+<slot />
