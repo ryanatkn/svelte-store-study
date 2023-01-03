402a03a4d3d723f9f925df71dcf7931293dfe02d
Ryan Atkinson <mail@ryanatkn.com>
2023-01-03 15:48:40 -0600

    add signal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
 <script lang="ts">
+	import {signal, computed} from '@preact/signals-core';
+
+	const counter = signal(0);
+	const doubled = computed(() => counter.value * 2);
+
+	const click = () => {
+		counter.value++;
+	};
 </script>
 
 <main class="markup column">
@@ -7,6 +15,11 @@
 			<h1>Svelte store study</h1>
 			<div><a href="https://github.com/ryanatkn/svelte-store-study">source</a></div>
 		</header>
+	</section>
+	<section>
+		<button on:click={click}>clickme</button>
+		<div>counter: {$counter}</div>
+		<div>doubled: {$doubled}</div>
 	</section>
 </main>
 
